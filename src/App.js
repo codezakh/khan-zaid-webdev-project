cbce39553c9eaef44ae2424ac49738020e882413
Zaid Khan <brain@zaidkhan.me>
2017-08-08 01:46:35 -0400

    Commented out the request for keys, thatll go in backend.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import Keys from './App.config';
+// import Keys from './App.config';
 import 'reddit.js/reddit'
 import {parsePosts} from './RedditParser';
 import {Button, Row, Grid, FormControl} from 'react-bootstrap';
@@ -124,40 +124,40 @@ const SearchBar = ({search}) => {
 };
 
 
-const processMeme = (memeUrl) => {
-  let subscriptionKey = Keys.MicrosoftOCR;
-  let uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
-  let params = {
-    "language": "en",
-    "detectOrientation ": "true",
-  };
-  let ocr = $.ajax({
-    url: uriBase + "?" + $.param(params),
-
-    // Request headers.
-    beforeSend: function (jqXHR) {
-      jqXHR.setRequestHeader("Content-Type", "application/json");
-      jqXHR.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-    },
-
-    type: "POST",
-
-    // Request body.
-    data: '{"url": ' + '"' + memeUrl + '"}',
-  })
-
-    .done(function (data) {
-      // Show formatted JSON on webpage.
-      console.log(data);
-      return "it worked!!!";
-    })
-
-    .fail(function (jqXHR, textStatus, errorThrown) {
-      // Display error message.
-      return "something went wrong"
-    });
-  console.log(JSON.stringify(ocr));
-  return JSON.stringify(ocr);
-};
+// const processMeme = (memeUrl) => {
+//   let subscriptionKey = Keys.MicrosoftOCR;
+//   let uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+//   let params = {
+//     "language": "en",
+//     "detectOrientation ": "true",
+//   };
+//   let ocr = $.ajax({
+//     url: uriBase + "?" + $.param(params),
+//
+//     // Request headers.
+//     beforeSend: function (jqXHR) {
+//       jqXHR.setRequestHeader("Content-Type", "application/json");
+//       jqXHR.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+//     },
+//
+//     type: "POST",
+//
+//     // Request body.
+//     data: '{"url": ' + '"' + memeUrl + '"}',
+//   })
+//
+//     .done(function (data) {
+//       // Show formatted JSON on webpage.
+//       console.log(data);
+//       return "it worked!!!";
+//     })
+//
+//     .fail(function (jqXHR, textStatus, errorThrown) {
+//       // Display error message.
+//       return "something went wrong"
+//     });
+//   console.log(JSON.stringify(ocr));
+//   return JSON.stringify(ocr);
+// };
 
 export default App;
